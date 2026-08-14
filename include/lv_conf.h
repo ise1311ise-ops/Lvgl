e06ae1586a97e328ce80c87aefc4fd55cd3f79f6
ise1311ise-ops <ise1311ise@gmail.com>
2026-08-14 17:19:09 +0300

    Add lv_conf.h with configuration settings

---
+++ include/lv_conf.h
@@ -0,0 +1,15 @@
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#define LV_COLOR_DEPTH 16
+
+#define LV_MEM_SIZE (64U * 1024U)
+
+#define LV_USE_LOG 0
+
+#define LV_USE_LABEL 1
+#define LV_USE_BTN 1
+
+#define LV_TICK_CUSTOM 1
+
+#endif
